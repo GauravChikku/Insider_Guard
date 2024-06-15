4e5ac318a18abf93348452fcd6105e982ee0aada
GauravChikku <126683142+GauravChikku@users.noreply.github.com>
2024-06-15 13:09:14 +0530

    Delete backend directory

--- backend/app/models/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    activityLogs: { type: Array, default: [] }
-});
-
-module.exports = mongoose.model('User', userSchema);
